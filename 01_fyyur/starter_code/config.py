c149a4fca48c770c126d61bd8d4841279bb3c8b3
marait123 <max.ibrahim43@hotmail.com>
2022-01-13 21:37:15 -0800

    initial commit

---
+++ 01_fyyur/starter_code/config.py
@@ -0,0 +1,35 @@
+import os
+SECRET_KEY = os.urandom(32)
+# Grabs the folder where the script runs.
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Enable debug mode.
+DEBUG = True
+
+# Connect to the database
+
+
+# TODO IMPLEMENT DATABASE URL
+SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+#----------------------------------------------------------------------------#
+# Imports
+#----------------------------------------------------------------------------#
+
+import json
+import dateutil.parser
+import babel
+from flask import Flask, render_template, request, Response, flash, redirect, url_for
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+import logging
+from logging import Formatter, FileHandler
+from flask_wtf import Form
+from forms import *
+#----------------------------------------------------------------------------#
+# App Config.
+#----------------------------------------------------------------------------#
+
+app = Flask(__name__)
+moment = Moment(app)
+app.config.from_object('config')
+db = SQLAlchemy(app)
